aa3a9a1f235404a07cce5bd84f3e2fe12b9a8cb2
Benjamin BENOIT <benjaminbenoit@MacBook-Air-de-Benjamin.local>
2025-11-03 12:42:25 +0100

    ajout redirection avec petit slug fix 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainApp from "./routes/MainApp";
 import MapsViewer from "./routes/MapsViewer";
-import RedirectSlugPage from "./routes/RedirectSlugPage"; // 👈 ajout
+import RedirectSlugPage from "./routes/RedirectSlugPage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainApp />} />
+        {/* 🔧 Page principale uniquement si on est exactement sur / */}
+        <Route index element={<MainApp />} />
+
+        {/* 🔗 Redirection courte */}
+        <Route path="/m/:slug" element={<RedirectSlugPage />} />
+
+        {/* 🗺️ Visualisation carte */}
         <Route path="/maps" element={<MapsViewer />} />
-        <Route path="/m/:slug" element={<RedirectSlugPage />} /> {/* 👈 ajout */}
+
+        {/* 🧭 Catch-all pour erreurs 404 éventuelles */}
+        <Route path="*" element={<div>Page introuvable</div>} />
       </Routes>
     </Router>
   );
